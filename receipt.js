65fce06a43377f1deef0c0add9615d7dbd221c04
Zero <zero117x@protonmail.ch>
2022-12-02 10:16:31 -0500

    Minor JS change for merge testing

--- receipt.js
+++ receipt.js
@@ -5,37 +5,37 @@ const receiptTotalElement = document.getElementById('receiptTotal');
 var receiptSubTotal = 0;
 var tax = 0;
 
-// var receiptItems = JSON.parse(localStorage.getItem("finalCart"));
+var receiptItems = JSON.parse(localStorage.getItem("finalCart"));
 
-var receiptItems = [
-    {
-        id: 0,
-        name: "Burrito de Bistec",
-        price: 9.99,
-        image: "bandidos_images/burrito_de_bistec.jpg",
-        description: "Flour tortilla stuffed with steak and choice of toppings",
-        calories: 920,
-        quantity: 1
-    },
-    {
-        id: 1,
-        name: "Burrito de Carnitas",
-        price: 8.99,
-        image: "bandidos_images/burrito_de_carnitas.jpg",
-        description: "Flour tortilla stuffed with pork and choice of toppings",
-        calories: 920,
-        quantity: 2
-    },
-    {
-        id: 2,
-        name: "Burrito de Chorizo",
-        price: 8.99,
-        image: "bandidos_images/burrito_de_chorizo.jpg",
-        description: "Flour tortilla stuffed with mexican sausage and choice of toppings",
-        calories: 920,
-        quantity: 5
-    }
-];
+// var receiptItems = [
+//     {
+//         id: 0,
+//         name: "Burrito de Bistec",
+//         price: 9.99,
+//         image: "bandidos_images/burrito_de_bistec.jpg",
+//         description: "Flour tortilla stuffed with steak and choice of toppings",
+//         calories: 920,
+//         quantity: 1
+//     },
+//     {
+//         id: 1,
+//         name: "Burrito de Carnitas",
+//         price: 8.99,
+//         image: "bandidos_images/burrito_de_carnitas.jpg",
+//         description: "Flour tortilla stuffed with pork and choice of toppings",
+//         calories: 920,
+//         quantity: 2
+//     },
+//     {
+//         id: 2,
+//         name: "Burrito de Chorizo",
+//         price: 8.99,
+//         image: "bandidos_images/burrito_de_chorizo.jpg",
+//         description: "Flour tortilla stuffed with mexican sausage and choice of toppings",
+//         calories: 920,
+//         quantity: 5
+//     }
+// ];
 
 function renderReceiptItems() {
     receiptItemsContainer.innerHTML = "";
